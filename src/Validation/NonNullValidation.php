c94e273e0bcbe31d06508925a25a2e7a08348c8b
Necip Allef <necipallef@gmail.com>
2018-03-15 09:34:16 +0300

    ADD NonNullValidation class

---
+++ src/Validation/NonNullValidation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Fabs\Component\Validator\Validation;
+
+use Fabs\Component\Validator\Exception\Exception;
+
+class NonNullValidation extends ValidationBase
+{
+    /**
+     * @param string $non_null_value
+     * @return bool
+     */
+    function isValidated($non_null_value)
+    {
+        // For now parameter non_null_value is guaranteed to be non-null
+        // but this may change in the future, so check if value is not null
+        // just to be safe.
+        return $non_null_value !== null;
+    }
+
+    /**
+     * @param bool $allow_null
+     * @return ValidationBase|void
+     * @throws Exception
+     */
+    public function setAllowNull($allow_null)
+    {
+        $class_name = NonNullValidation::class;
+        throw new Exception("cannot call setAllowNull on ${class_name}");
+    }
+
+    /**
+     * @return bool
+     */
+    public function getAllowNull()
+    {
+        return false;
+    }
+}
