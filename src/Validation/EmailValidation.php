0adb8b7300780f32beabedde0d59588fa45cdb80
omerkocaoglu <okocaoglu05@gmail.com>
2018-10-25 23:59:33 +0300

    FIX EmailValidation ADD EmailValidation Tests

--- src/Validation/EmailValidation.php
+++ src/Validation/EmailValidation.php
@@ -2,11 +2,46 @@
 
 namespace Fabstract\Component\Validator\Validation;
 
+/**
+ * Class EmailValidation
+ * @package Fabstract\Component\Validator\Validation
+ * @see \Fabstract\Component\Validator\Test\PHPUnit\EmailValidation\IsValidMethodTest
+ *
+ * VALID VALUES
+ * example@example.com
+ * Example@example.com
+ * 123example@example.com
+ * ex123ample@example.com
+ * e_xample@example.com
+ * e-xample@example.com
+ * exampl.e@example.com
+ * exa.mple@example.com
+ * exa'mple@example.com
+ * e'xample@example.com
+ * exa&mple@example.com
+ * exa+mple@example.com
+ * example@Example.com
+ * example@EXAMPLE.com
+ * example@exa-mple.com
+ * example@example.com.uk
+ * example@example.co
+ *
+ * INVALID VALUES
+ * exa!mple@example.com
+ * exa?mple@example.com
+ * exa|mple@example.com
+ * e..xample@example.com
+ * example@_example.com
+ * example@-example.com
+ * example@example..com
+ * e@xample@example.com
+ * exa$mple@example.com
+ */
 class EmailValidation extends PatternValidation
 {
     public final function __construct()
     {
-        $pattern = '/^\w.+@\w+\.[a-zA-Z]{2,}$/';
+        $pattern = '/^[^\W_]+[\.\-\\\'\+\&_]?[^\W_]+@[^\W_]+[\-\&\\\'_]?[^\W_]+\.[^\W_]{2,}(?:\.[^\W_]{2})?$/';
         parent::__construct($pattern);
     }
 
---
+++ test/PHPUnit/EmailValidation/IsValidMethodTest.php
@@ -0,0 +1,430 @@
+<?php
+
+namespace Fabstract\Component\Validator\Test\PHPUnit\EmailValidation;
+
+use Fabstract\Component\UnitTest\MethodTestBase;
+use Fabstract\Component\Validator\Validation\EmailValidation;
+
+class IsValidMethodTest extends MethodTestBase
+{
+    #region [correct arguments]
+
+    public function testNullEqualsTrueWhenAllowNullIsTrue()
+    {
+        $arguments = [null];
+
+        $return = $this->call(EmailValidation::create()->setAllowNull(true), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartStartWithAlphaNumericLowerCharacterReturnsTrue()
+    {
+        $arguments = ['example@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartStartWithAlphaNumericUpperCharacterReturnsTrue()
+    {
+        $arguments = ['Example@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartStartWithAlphaNumericNumberReturnsTrue()
+    {
+        $arguments = ['123example@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartIncludeNumberReturnsTrue()
+    {
+        $arguments = ['ex123ample@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartIncludeUnderscoreReturnsTrue()
+    {
+        $arguments = ['e_xample@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartIncludeMinusReturnsTrue()
+    {
+        $arguments = ['e-xample@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartIncludeDotReturnsTrue()
+    {
+        $arguments = ['exa.mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartIncludeSingleQuoteReturnsTrue()
+    {
+        $arguments = ['e\'xample@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartIncludeAmpersandReturnsTrue()
+    {
+        $arguments = ['exa&mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLocalPartIncludePlusReturnsTrue()
+    {
+        $arguments = ['exa+mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testDomainPartStartWithUppercaseCharacterReturnsTrue()
+    {
+        $arguments = ['example@Example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testDomainPartCompletelyUppercaseCharactersReturnsTrue()
+    {
+        $arguments = ['example@EXAMPLE.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testDomainPartIncludeMinusMarkReturnsTrue()
+    {
+        $arguments = ['example@exa-mple.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testEmailAddressAllowCountrySuffixReturnsTrue()
+    {
+        $arguments = ['example@example.com.uk'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+    public function testLastPartTwoLengthReturnsTrue()
+    {
+        $arguments = ['example@example.co'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(true, $return);
+    }
+
+#endregion
+
+
+#region [incorrect arguments]
+
+    public function testLocalPartIncludeSpaceReturnsFalse()
+    {
+        $arguments = ['exa mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeExclamationReturnsFalse()
+    {
+        $arguments = ['exa!mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeDoubleQuotesReturnsFalse()
+    {
+        $arguments = ['exa""mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludePoundReturnsFalse()
+    {
+        $arguments = ['exa#mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeDollarReturnsFalse()
+    {
+        $arguments = ['exa$mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludePercentReturnsFalse()
+    {
+        $arguments = ['exa%mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeOpeningParenthesisReturnsFalse()
+    {
+        $arguments = ['exa(mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeClosingParenthesisReturnsFalse()
+    {
+        $arguments = ['exa)mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeCommaReturnsFalse()
+    {
+        $arguments = ['exa,mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeColonReturnsFalse()
+    {
+        $arguments = ['exa:mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeSemiColonReturnsFalse()
+    {
+        $arguments = ['exa;mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeLessThanReturnsFalse()
+    {
+        $arguments = ['exa<mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeGreaterThanReturnsFalse()
+    {
+        $arguments = ['exa>mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeQuestionMarkReturnsFalse()
+    {
+        $arguments = ['exa?mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeAtMarkReturnsFalse()
+    {
+        $arguments = ['exa@mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeBackslashReturnsFalse()
+    {
+        $arguments = ['exa\mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeOpeningBracketReturnsFalse()
+    {
+        $arguments = ['exa[mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludeClosingBracketReturnsFalse()
+    {
+        $arguments = ['exa]mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartIncludePipeReturnsFalse()
+    {
+        $arguments = ['exa|mple@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartDoubleDotReturnsFalseReturnsFalse()
+    {
+        $arguments = ['e..xample@example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLocalPartWithoutAtMarkReturnsFalse()
+    {
+        $arguments = ['example.example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testDomainPartStartWithUnderscoreReturnsFalse()
+    {
+        $arguments = ['example@_example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testDomainPartStartWithNonAlphaNumericCharacterReturnsFalse()
+    {
+        $arguments = ['example@-example.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testDomainPartEndWithUnderscoreReturnsFalse()
+    {
+        $arguments = ['example@example_.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testDomainPartEndWithNonAlphaNumericCharacterReturnsFalse()
+    {
+        $arguments = ['example@example-.com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testDomainPartDoesNotIncludeDotReturnsFalse()
+    {
+        $arguments = ['example@examplecom'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testDomainPartIncludesMoreThanOneDotReturnsFalse()
+    {
+        $arguments = ['example@example..com'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testEmailAddressStartWithSpaceReturnsFalse()
+    {
+        $arguments = [' example@example.com.uk'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLastPartMoreThanTwoLengthReturnsFalse()
+    {
+        $arguments = ['example@example.com.abc'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+
+    public function testLastPartWithNonWordCharacterReturnsFalse()
+    {
+        $arguments = ['example@example.co^m'];
+
+        $return = $this->call(new EmailValidation(), $arguments);
+
+        $this->assertEquals(false, $return);
+    }
+}
